cc850350c6c290151e57162369fb736f26053c71
Hiroya Onoe <git@onoe.dev>
2023-11-07 00:35:42 +0900

    page table

--- kernel.c
+++ kernel.c
@@ -7,10 +7,41 @@ typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
+extern char __kernel_base[];
 
 struct process procs[PROCS_MAX];
 struct process *current_proc;
 struct process *idle_proc;
+
+paddr_t alloc_pages(uint32_t n) {
+	static paddr_t next_paddr = (paddr_t) __free_ram;
+	paddr_t paddr = next_paddr;
+	next_paddr += n * PAGE_SIZE;
+
+	if (next_paddr > (paddr_t) __free_ram_end)
+		PANIC("out of memory");
+
+	memset((void *) paddr, 0, n * PAGE_SIZE);
+	return paddr;
+}
+
+void map_page(uint32_t *table1, vaddr_t vaddr, paddr_t paddr, uint32_t flags) {
+	if (!is_aligned(vaddr, PAGE_SIZE))
+		PANIC("unaligned vaddr %x", vaddr);
+
+	if (!is_aligned(paddr, PAGE_SIZE))
+		PANIC("unaligned paddr %x", paddr);
+
+	uint32_t vpn1 = (vaddr >> 22) & 0x3ff;
+	if ((table1[vpn1] & PAGE_V) == 0) {
+		uint32_t pt_paddr = alloc_pages(1);
+		table1[vpn1] = ((pt_paddr / PAGE_SIZE) << 10) | PAGE_V;
+	}
+
+	uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
+	uint32_t *table0 = (uint32_t *) ((table1[vpn1] >> 10) * PAGE_SIZE);
+	table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
+}
 
 __attribute__((naked))
 void switch_context(uint32_t *prev_sp, uint32_t *next_sp) {
@@ -77,9 +108,16 @@ struct process *create_process(uint32_t pc) {
 	*--sp = 0;
 	*--sp = (uint32_t) pc;
 
+	uint32_t *page_table = (uint32_t *) alloc_pages(1);
+
+	for (paddr_t paddr = (paddr_t) __kernel_base;
+			paddr < (paddr_t) __free_ram_end; paddr += PAGE_SIZE)
+			map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
+
 	proc->pid = i + 1;
 	proc->state = PROC_RUNNABLE;
 	proc->sp = (uint32_t) sp;
+	proc->page_table = page_table;
 	return proc;
 }
 
@@ -97,9 +135,13 @@ void yield(void) {
 		return;
 
 	__asm__ __volatile__ (
+		"sfence.vma\n"
+		"csrw satp, %[satp]\n"
+		"sfence.vma\n"
 		"csrw sscratch, %[sscratch]\n"
 		:
-		: [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+		: [satp] "r" (SATP_SV32 | ((uint32_t) next->page_table / PAGE_SIZE)),
+			[sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
 	);
 
 	struct process *prev = current_proc;
@@ -126,18 +168,6 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, lo
 
 void putchar(char ch) {
 	sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
-}
-
-paddr_t alloc_pages(uint32_t n) {
-	static paddr_t next_paddr = (paddr_t) __free_ram;
-	paddr_t paddr = next_paddr;
-	next_paddr += n * PAGE_SIZE;
-
-	if (next_paddr > (paddr_t) __free_ram_end)
-		PANIC("out of memory");
-
-	memset((void *) paddr, 0, n * PAGE_SIZE);
-	return paddr;
 }
 
 void handle_trap(struct trap_frame *f) {
