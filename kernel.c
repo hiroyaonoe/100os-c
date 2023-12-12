068b63e5ef20f6e3f8f8cd8c33f944604891ad4d
Hiroya Onoe <git@onoe.dev>
2023-12-12 22:25:07 +0900

    syscall

--- kernel.c
+++ kernel.c
@@ -188,12 +188,49 @@ void putchar(char ch) {
 	sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
 }
 
+long getchar(void) {
+    struct sbiret ret = sbi_call(0, 0, 0, 0, 0, 0, 0, 2);
+    return ret.error;
+}
+
+void handle_syscall(struct trap_frame *f) {
+	switch (f->a3) {
+		case SYS_PUTCHAR:
+			putchar(f->a0);
+			break;
+		case SYS_GETCHAR:
+			while (1) {
+				long ch = getchar();
+				if (ch >= 0) {
+					f->a0 = ch;
+					break;
+				}
+				yield();
+			}
+			break;
+		case SYS_EXIT:
+			printf("process %d exited\n", current_proc->pid);
+			current_proc->state = PROC_EXITED;
+			yield();
+			PANIC("unreachable");
+		default:
+		PANIC("unexpected syscall a3=%x\n", f->a3);
+	}
+}
+
 void handle_trap(struct trap_frame *f) {
 	uint32_t scause = READ_CSR(scause);
 	uint32_t stval = READ_CSR(stval);
 	uint32_t user_pc = READ_CSR(sepc);
 
-	PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
+	if (scause == SCAUSE_ECALL) {
+		handle_syscall(f);
+		user_pc += 4;
+	} else {
+		PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
+	}
+
+	WRITE_CSR(sepc, user_pc);
 }
 
 __attribute__((naked))
